7612f35e232065f727fcbb3fb58368b3ccb8f3fc
Esa Sahla <esa.sahla@gavon.fi>
2022-06-07 14:28:02 +0300

    Changed the paths to geocode and forecast

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
-const geocode = require(".\\utils\\geocode.js");
-const forecast = require(".\\utils\\forecast.js");
+const geocode = require("./utils/geocode.js");
+const forecast = require("./utils/forecast.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
